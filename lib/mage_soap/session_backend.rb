80c58c5395bf0dc3a438ad92fcb4cdab4d2dfced
Marian Theisen <marian.theisen@kayoom.com>
2016-03-10 15:55:41 +0100

    remove duplication

--- lib/mage_soap/session_backend.rb
+++ lib/mage_soap/session_backend.rb
@@ -7,10 +7,6 @@ module MageSoap
   class SessionBackend < AsyncHttpBackend
     def initialize(user, password, root_module, session_provider: nil, connection: Faraday.new, executor: nil)
       super(root_module, connection: connection, executor: executor)
-      @root_module = root_module
-      @api_class = @root_module.const_get :Api
-      @service_class = @root_module.const_get :MagentoService
-      @proxy_class = @service_class.const_get :MageApiModelServerV2HandlerPortProxy
 
       session_provider ||= LoginSessionProvider.new(
         LoginBackend.new(root_module, connection: connection, executor: :immediate)
