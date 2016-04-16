55eabe1ad33ff684a59fd88c61f471a829b18b30
Marian Theisen <marian.theisen@kayoom.com>
2016-04-16 14:26:50 +0200

    some constants for the api

---
+++ lib/mage_soap/api_constants.rb
@@ -0,0 +1,11 @@
+module MageSoap
+  module ApiConstants
+    VISIBILITY_BOTH = 4
+    VISIBILITY_IN_CATALOG = 2
+    VISIBILITY_IN_SEARCH = 3
+    VISIBILITY_NOT_VISIBLE = 1
+
+    STATUS_DISABLED = 2
+    STATUS_ENABLED = 1
+  end
+end
